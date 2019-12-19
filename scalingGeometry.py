14ded3c6a156b75b4eba58030df07266bf1a6eb9
marcelo <marcelosalles@github.com>
2019-12-19 16:27:43 -0300

    working on comments

--- scalingGeometry.py
+++ scalingGeometry.py
@@ -1,3 +1,4 @@
+
 import json
 import os
 
@@ -42,7 +43,7 @@ def ref_vertex(vertex, vertex_list, scale,ref_scale):
     return(new_vertex)
 
 def scaling(scalex=2, scaley=2,scalez=2, ref=False, window_scale=True,
-    shading_scale=False, input_file='model.epJSON',output_name='scaled_model.epJSON'):
+    shading_scale=True, input_file='model.epJSON',output_name='scaled_model.epJSON'):
     # This function multiplies the vertices of the EnergyPlus model by a
     # determined value, and returns the scaled model (.idf and .epJSON)
     
@@ -69,7 +70,7 @@ def scaling(scalex=2, scaley=2,scalez=2, ref=False, window_scale=True,
     content = json.load(file)
     file.close()
 
-    # changing epJSON fields
+    #### Changing epJSON fields ####
             
     ## Zone
     for i in list(content["Zone"].keys()):
@@ -104,6 +105,7 @@ def scaling(scalex=2, scaley=2,scalez=2, ref=False, window_scale=True,
         for i in list(content["AirflowNetwork:SimulationControl"].keys()):
             content["AirflowNetwork:SimulationControl"][i]["ratio_of_building_width_along_short_axis_to_width_along_long_axis"] =  ratio_of_building_afn
 
+    # Defines opening areas according to reference
     if ref:
 
         # erase shading devices
@@ -143,7 +145,7 @@ def scaling(scalex=2, scaley=2,scalez=2, ref=False, window_scale=True,
         for fenestration in list(content["FenestrationSurface:Detailed"].keys()):
             if content["FenestrationSurface:Detailed"][fenestration]['surface_type'] == 'Window':
 
-                # take vertices whichi will be considered
+                # take vertices which will be considered
                 x_vertex = []
                 y_vertex = []
                 z_vertex = []
@@ -173,11 +175,11 @@ def scaling(scalex=2, scaley=2,scalez=2, ref=False, window_scale=True,
                         if surface == content["FenestrationSurface:Detailed"][fenestration]['building_surface_name']:
                             zones[zone]['fenestrations'][fenestration] = window_area
 
-        # print(zones)
+        
+    ## FenestrationSurface:Detailed
         # defines windows sizes and scale factor to create ref openings
         for zone in zones.keys():
             zones[zone]['ref_window_area'] = zones[zone]['area']*REF_WINDOW_TO_FLOOR_RATIO
-            # print(zones[zone]['ref_window_area'])
 
             real_window_area = 0
             for fenestration in zones[zone]['fenestrations'].keys():
@@ -186,6 +188,7 @@ def scaling(scalex=2, scaley=2,scalez=2, ref=False, window_scale=True,
             zones[zone]['real_window_area'] = real_window_area
             zones[zone]['ref_real_ratio'] = zones[zone]['ref_window_area']/zones[zone]['real_window_area']
 
+        # changes values of windows' vertices
         for i in list(content["FenestrationSurface:Detailed"].keys()):
 
             x_vertex = []
@@ -209,7 +212,6 @@ def scaling(scalex=2, scaley=2,scalez=2, ref=False, window_scale=True,
             if content["FenestrationSurface:Detailed"][i]['surface_type'] == 'Window':
 
                 content["FenestrationSurface:Detailed"][i]["vertex_1_x_coordinate"] = ref_vertex(content["FenestrationSurface:Detailed"][i]["vertex_1_x_coordinate"],x_vertex,scalex,ref_scale)
-                # print(content["FenestrationSurface:Detailed"][i]["vertex_1_x_coordinate"])
                 content["FenestrationSurface:Detailed"][i]["vertex_2_x_coordinate"] = ref_vertex(content["FenestrationSurface:Detailed"][i]["vertex_2_x_coordinate"],x_vertex,scalex,ref_scale)
                 content["FenestrationSurface:Detailed"][i]["vertex_3_x_coordinate"] = ref_vertex(content["FenestrationSurface:Detailed"][i]["vertex_3_x_coordinate"],x_vertex,scalex,ref_scale)
                 content["FenestrationSurface:Detailed"][i]["vertex_4_x_coordinate"] = ref_vertex(content["FenestrationSurface:Detailed"][i]["vertex_4_x_coordinate"],x_vertex,scalex,ref_scale)
@@ -235,7 +237,7 @@ def scaling(scalex=2, scaley=2,scalez=2, ref=False, window_scale=True,
                 content["FenestrationSurface:Detailed"][i]["vertex_3_y_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_3_y_coordinate"],y_vertex,scaley)
                 content["FenestrationSurface:Detailed"][i]["vertex_4_y_coordinate"] = constant_vertex(content["FenestrationSurface:Detailed"][i]["vertex_4_y_coordinate"],y_vertex,scaley)
 
-    ## FenestrationSurface:Detailed
+    # window area changes according to geometry scales
     elif window_scale:
         for i in list(content["FenestrationSurface:Detailed"].keys()):
             
@@ -254,6 +256,7 @@ def scaling(scalex=2, scaley=2,scalez=2, ref=False, window_scale=True,
             content["FenestrationSurface:Detailed"][i]["vertex_3_z_coordinate"] = content["FenestrationSurface:Detailed"][i]["vertex_3_z_coordinate"]*scalez
             content["FenestrationSurface:Detailed"][i]["vertex_4_z_coordinate"] = content["FenestrationSurface:Detailed"][i]["vertex_4_z_coordinate"]*scalez
 
+    # window area does not change
     else:
         for i in list(content["FenestrationSurface:Detailed"].keys()):
 
@@ -300,29 +303,14 @@ def scaling(scalex=2, scaley=2,scalez=2, ref=False, window_scale=True,
 
     ## Shading:Building:Detailed
     if not ref:
-        if shading_scale:
-            for i in list(content["Shading:Building:Detailed"].keys()):
-                for j,k in enumerate(content["Shading:Building:Detailed"][i]["vertices"]):
-                    content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_x_coordinate"] = content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_x_coordinate"]*scalex
-                    content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_y_coordinate"] = content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_y_coordinate"]*scaley
-                    content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_z_coordinate"] = content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_z_coordinate"]*scalez
-
-        else:
-            for i in list(content["Shading:Building:Detailed"].keys()):
-
-                x_vertex = []
-                y_vertex = []
-                z_vertex = []
-
-                for j,k in enumerate(content["Shading:Building:Detailed"][i]["vertices"]):
-                    x_vertex.append(content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_x_coordinate"])
-                    y_vertex.append(content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_y_coordinate"])
-                    z_vertex.append(content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_z_coordinate"])
-
-                for j,k in enumerate(content["Shading:Building:Detailed"][i]["vertices"]):
-                    content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_x_coordinate"] = constant_vertex(content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_x_coordinate"],x_vertex,scalex)
-                    content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_y_coordinate"] = constant_vertex(content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_y_coordinate"],y_vertex,scaley)
-                    content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_z_coordinate"] = constant_vertex(content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_z_coordinate"],z_vertex,scalez)
+        # erase shading devices
+        if 'Shading:Building:Detailed' in list(content.keys()):
+            if shading_scale:  # at the moment, code does not work for "shading_scale = False"
+                for i in list(content["Shading:Building:Detailed"].keys()):
+                    for j,k in enumerate(content["Shading:Building:Detailed"][i]["vertices"]):
+                        content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_x_coordinate"] = content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_x_coordinate"]*scalex
+                        content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_y_coordinate"] = content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_y_coordinate"]*scaley
+                        content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_z_coordinate"] = content["Shading:Building:Detailed"][i]["vertices"][j]["vertex_z_coordinate"]*scalez
 
     # writing  epJSON file
     file = open(output_name, "w")
@@ -333,7 +321,7 @@ def scaling(scalex=2, scaley=2,scalez=2, ref=False, window_scale=True,
     os.system('energyplus -x -c '+output_name)
     os.system('rm eplusout.*')
 
-## Test function changing values on the following lines: ---------------
+######## Test function changing values on the following lines ########
 
 # # Define the name of the input file here
 # FILE_NAME = 'vn_Caso2.idf'
@@ -357,25 +345,11 @@ def scaling(scalex=2, scaley=2,scalez=2, ref=False, window_scale=True,
 
 
 '''
-
-- abre o idf
-- converte pra json
-- enumera as zonas
-    procurando no objeto "Zone"
-- calcula as areas das zonas
-    procurando em "BuildingSurface:Detailed", "surface_type": "Floor", e lendo os vertices
-- enumera as surfaces de cada zona
-    procurando em "BuildingSurface:Detailed", e adicionando a lista das surfaces, pelo "zone_name" (só walls)
-- enumera as janelas de cada zona, a partir das surfaces
-    para cada zona, lendo "building_surface_name" e "surface_type": "Window" do "FenestrationSurface:Detailed"
-- calcula as áreas das janelas
-    lendo os vertices das janelas (conferir se varia no x ou y)
-- corrige as áreas das janelas
-    multiplicando W (ou H) pelo fator "area ref/area real"
+O que pode ser melhorado:
+- Janelas:
     *conferir se W (ou H) não passa da aresta da Wall
     *conferir se a janela não se sobrepoem com a porta
     **definir se vai consertar autmaticamente, ou só avisar
-- salva o json
-- converte pra idf
-
+- Shading:
+    Encontrar uma forma de acompanhar a geometria, sem mudar o ângulo de sombreamento
 '''
