fb012f931174f2b5c7904885c124038ef36d443c
marcelo <marcelosalles@github.com>
2020-03-05 17:23:47 -0300

    superhero again

--- superheroes.R
+++ superheroes.R
@@ -3,9 +3,36 @@ library(ggplot2)
 setwd('~/codes')
 #
 
-df = read.csv('outputs_uni_uh.csv')
-df$phft_dif = df$phft - df$phft_ref
+strip = function(string, position=4){
+  # para identificar características de geometria a partir do nome do slice
+  as.character(strsplit(as.character(df$geometria[2]),'_')[[1]][4])
+}
 
+df = read.csv('outputs_uni_uh.csv')
+
+# diferencas entre caso e ref
+df$phft_dif = df$phft - df$phft_ref
+df$cgtr_dif = df$cgtr_cooling + df$cgtr_heating - df$cgtr_cooling_ref - df$cgtr_heating_ref
+
+#area da uh base
+area_uh = 38.58
+
+# identifica caracteristicas a partir do nome do slice
+df$ratio = sapply(df$geometria, strip, position = 4)  # strsplit(as.character(df$geometria),'_')[4]
+df$height = sapply(df$geometria, strip, position = 5)  # strsplit(as.character(df$geometria),'_')[[1]][5]
+df$area = sapply(df$geometria, strip, position = 6)  # strsplit(as.character(df$geometria),'_')[[1]][6]
+df$area = ifelse(
+  substr(df$area,1,1) == '0', area_uh, ifelse(
+    substr(df$area,1,1) == '1', 1.5*area_uh,
+    2*area_uh
+  )
+)
+df$cgtr_dif = df$cgtr_dif/df$area
+
+df$somb = sapply(df$sombreamento, strip, position = 3)  # strsplit(as.character(df$sombreamento),'_')[[1]][3]
+df$wwr = sapply(df$paf, strip, position = 3)  # strsplit(as.character(df$paf),'_')[[1]][3]
+
+# separe por estado
 df_SC = df[df$estado == 'SC',]
 df_PR = df[df$estado == 'PR',]
 df_RS = df[df$estado == 'RS',]
@@ -15,55 +42,41 @@ df_MG = df[df$estado == 'MG',]
 df_RJ = df[df$estado == 'RJ',]
 df_TO = df[df$estado == 'TO',]
 
+# pega os 5% melhores de cada estado
 df_heroes = data.frame()
 
 for(uf in unique(df$estado)){
   df_UF = df[df$estado == uf,]
-  df_heroes = rbind(df_heroes, subset(df_UF, df_UF$phft_dif > quantile(df_UF$phft_dif, .95)))  # max(df_UF$phft_dif)
-  
-  # df_plot = df_UF[df_UF$phft_dif > quantile(df_UF$phft_dif, .95),]
-  # 
-  # ggplot(df_plot, aes(df_plot$phft)) +
-  #   geom_histogram(binwidth = .05)
-  # 
-  # ggplot(df_plot, aes(df_plot$phft_dif, fill = df_plot$absortancia)) +
-  #   geom_histogram(binwidth = .01)
-  # 
-  # ggplot(df_plot, aes(df_plot$phft_dif, fill = df_plot$open_fac)) +
-  #   geom_histogram(binwidth = .01)
-  # 
+  df_heroes = rbind(df_heroes, subset(df_UF, df_UF$phft_dif > quantile(df_UF$phft_dif, .95)))
+  # df_heroes = rbind(df_heroes, subset(df_UF, df_UF$cgtr_dif < quantile(df_UF$cgtr_dif, .05)))
 }
 
-strip = function(string, position=4){
-  as.character(strsplit(as.character(string),'_')[[1]][position])
-}
-strip2 = function(line, column = 'geometria', position=4){
-  as.character(strsplit(as.character(line[column]),'_')[[1]][position])
-}
+# para facilitar, chamando sempre o "df"
+df_base = df
+# df = df_base
+df_heroes_phft = df_heroes
+# df_heroes_cgtr = df_heroes
+df = df_heroes
 
-strip(df$geometria[2])
-strip2(as.character(df[3,]['wwr']))
-
-area = sapply(df$geometria, strip)
-
-df$ratio = sapply(df$geometria, strip, position = 4)  # strsplit(as.character(df$geometria),'_')[4]
-df$height = sapply(df$geometria, strip, position = 5)  # strsplit(as.character(df$geometria),'_')[[1]][5]
-df$area = sapply(df$geometria, strip, position = 6)  # strsplit(as.character(df$geometria),'_')[[1]][6]
-
-df$somb = sapply(df$sombreamento, strip, position = 3)  # strsplit(as.character(df$sombreamento),'_')[[1]][3]
-df$wwr = sapply(df$paf, strip, position = 3)  # strsplit(as.character(df$paf),'_')[[1]][3]
-
+# plota os casos separado por estado
 slices = c("ratio", "height", "area", "azimute", "veneziana", "componente", "absortancia", "vidro", "open_fac", "somb", "wwr")
+df$area = as.character(df$area)
 
 for(slice in slices){
+  png(filename = paste0(slice,'_phft.png'),
+  # png(filename = paste0(slice,'_cgtr.png'), 
+      width = 33.8, height = 19, units = "cm", res = 500)  
   plot(
     ggplot(df, aes(df[,slice])) +
     geom_bar() +  
-    ggtitle(slice) +
+    ggtitle(paste('PHft',slice)) +
+    # ggtitle(paste('CgTr',slice)) +
     theme(axis.text.x = element_text(angle=90)) +
     facet_grid(.~estado)
   )
+  dev.off()
 }
+#### ----
 
 ggplot(df, aes(df$cgtr_cooling_ref, df$cgtr_cooling)) +
   geom_point() +  #col(stat= 'identity') +
